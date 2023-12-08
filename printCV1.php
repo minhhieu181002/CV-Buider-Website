e7d0d98f5ec1f5d829441909c8421d7053a36485
nhatthanh.vu <nhatthanh.vu@veriserve-vietnam.com>
2023-12-08 10:26:07 +0700

    modify

--- printCV1.php
+++ printCV1.php
@@ -11,7 +11,6 @@ if ($mysqli->connect_error) {
 // Get the logged-in user's ID
 $userId = $_SESSION['id'];
 
-
 //Get data from resume table
 $sql = "SELECT id,name,full_name,date_of_birth,address,introduction,phone_number,certificate,mail,skill,avatar_url FROM resumes WHERE userId = ?";
 $stmt = $mysqli->prepare($sql);
@@ -34,8 +33,7 @@ if ($result->num_rows > 0) {
     $avt = $row['avatar_url'];
   }
 }
-// echo $avt;
-// echo $fullName;
+
 //Get data from education table
 $sql = "SELECT title,description FROM education WHERE resume_id = $resume_id";
 $stmt = $mysqli->prepare($sql);
@@ -56,9 +54,6 @@ for ($j = 0; $j < $i; $j++) {
   $titleEdu[$j] = $data[$j]['title'];
   $descriptionEdu[$j] = $data[$j]['description'];
 }
-// print_r($title[1]);
-// print_r($description[1]);
-
 
 //get data from experience table 
 $sql = "SELECT title, description FROM experience WHERE resume_id = $resume_id";
@@ -80,80 +75,90 @@ for ($j = 0; $j < $i; $j++) {
   $descriptionExp[$j] = $data[$j]['description'];
 }
 
-echo "
-  <!DOCTYPE html>
+// Function to generate HTML for title and description using array_map
+function generateHTML($titles, $descriptions)
+{
+  return array_map(function ($title, $description) {
+    return "
+      <h3>$title</h3>
+      <p>$description</p>
+      <br />
+    ";
+  }, $titles, $descriptions);
+}
+
+?>
+<!DOCTYPE html>
 <html lang='en'>
-  <!-- divinectorweb.com -->
-  <head>
-    <meta charset='UTF-8' />
-    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    <title>Responsive Resume website html css</title>
-    <link
-      href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap'
-      rel='stylesheet'
-    />
-    <link rel='stylesheet' href='/tem2/style2.css' />
-    <style>
+<!-- divinectorweb.com -->
+
+<head>
+  <meta charset='UTF-8' />
+  <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+  <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+  <title>Responsive Resume website html css</title>
+  <link href='https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap' rel='stylesheet' />
+  <link rel='stylesheet' href='/tem2/style2.css' />
+  <style>
     @media print {
       #printBtn {
         display: none;
       }
     }
   </style>
-  </head>
-  <body>
+</head>
+
+<body>
   <script>
-  function printDocument() {
-    window.print();
-    return false;
-  }
-</script>
-    <div class='container'>
-      <div class='header'>
-        <div class='img-area'>
-          <img src='$avt' alt='' />
-        </div>
-        <h1>$fullName</h1>
-        <h3>$nameOfCV</h3>
+    function printDocument() {
+      window.print();
+      return false;
+    }
+  </script>
+  <div class='container'>
+    <div class='header'>
+      <div class='img-area'>
+        <img src='<?php echo $avt; ?>' alt='' />
+      </div>
+      <h1><?php echo $fullName; ?></h1>
+      <h3><?php echo $nameOfCV; ?></h3>
+    </div>
+
+    <h2>Introduction</h2>
+    <p> <?php echo $intro; ?></p>
+
+    <div class='main'>
+      <div class='left'>
+        <h2>Personal Information</h2>
+        <p><strong>Name:</strong> <?php echo $fullName; ?></p>
+        <p><strong>Birthday:</strong> <?php echo $dateOfBirth; ?></p>
+        <p><strong>Email:</strong> <?php echo $mail; ?></p>
+        <p><strong>Phone:</strong> <?php echo $phone; ?></p>
+        <h2>Skills</h2>
+        <ul>
+          <?php echo $skill; ?>
+        </ul>
+        <h2>Education</h2>
+        <?php
+        // Generate HTML for education using generateHTML function
+        $educationHTML = generateHTML($titleEdu, $descriptionEdu);
+        echo implode("\n", $educationHTML);
+        ?>
       </div>
 
-      <div class='main'>
-        <div class='left'>
-          <h2>Personal Information</h2>
-          <p><strong>Name:</strong> $fullName</p>
-          <p><strong>Birthday:</strong> $dateOfBirth</p>
-          <p><strong>Email:</strong> $mail</p>
-          <p><strong>Phone:</strong> $phone</p>
-          <h2>Skills</h2>
-          <ul>
-            $skill
-          </ul>
-          <h2>Education</h2>
-          <h3>$titleEdu[0]</h3>
-          <p>$descriptionEdu[0]</p>
-          <br />
-          <h3>$titleEdu[1]</h3>
-          <p>$descriptionEdu[1]</p>
-        </div>
-
-      
-        <div class='right'>
-        <h2>Introduction</h2>
-        <p> $intro</p>
-          <h2>Work Experience</h2>
-          <h3>$titleExp[0]</h3>
-          <p> $descriptionExp[0]</p>
-          
-          <br />
-          <h3>$titleExp[1]</h3>
-          <p> $descriptionExp[1]</p>
-        </div>
+      <div class='right'>
+        <h2>Work Experience</h2>
+        <?php
+        // Generate HTML for work experience using generateHTML function
+        $experienceHTML = generateHTML($titleExp, $descriptionExp);
+        echo implode("\n", $experienceHTML);
+        ?>
       </div>
     </div>
-    <div class='printCV' style='display:flex; justify-content:center;align-items:center;' > 
+  </div>
+  <div class='printCV' style='display:flex; justify-content:center;align-items:center;'>
     <a id='printBtn' style='padding: 20px 40px; background-color: #547638; text-decoration:none; border-radius:10px; color:#fff;' href='#' onclick='printDocument();'>Print PDF</a>
   </div>
-  </body>
+</body>
+
 </html>
-";
--- printCV2.php
+++ printCV2.php
@@ -11,9 +11,8 @@ if ($mysqli->connect_error) {
 // Get the logged-in user's ID
 $userId = $_SESSION['id'];
 
-
-//Get data from resume table
-$sql = "SELECT id,name,full_name,date_of_birth,address,introduction,phone_number,certificate,mail,skill,avatar_url FROM resumes WHERE userId = ?";
+// Get data from resume table
+$sql = "SELECT id, name, full_name, date_of_birth, address, introduction, phone_number, certificate, mail, skill, avatar_url FROM resumes WHERE userId = ?";
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param('i', $userId);
 $stmt->execute();
@@ -34,204 +33,157 @@ if ($result->num_rows > 0) {
     $avt = $row['avatar_url'];
   }
 }
-// echo $avt;
-// echo $fullName;
-//Get data from education table
-$sql = "SELECT title,description FROM education WHERE resume_id = $resume_id";
-$stmt = $mysqli->prepare($sql);
-//$stmt->bind_param('i', $resume_id);
-$stmt->execute();
-$result = $stmt->get_result();
 
+// Get data from education table
+$sql = "SELECT title, description FROM education WHERE resume_id = $resume_id";
+$result = $mysqli->query($sql);
+$educationData = array(); // Create an array to store education data
 if ($result->num_rows > 0) {
-  $data = array();
-  $i = 0;
   while ($row = $result->fetch_assoc()) {
-    $data[$i++] = $row;
+    $educationData[] = $row; // Populate the educationData array
   }
 }
-$titleEdu = array();
-$descriptionEdu = array();
-for ($j = 0; $j < $i; $j++) {
-  $titleEdu[$j] = $data[$j]['title'];
-  $descriptionEdu[$j] = $data[$j]['description'];
-}
-// print_r($title[1]);
-// print_r($description[1]);
 
-
-//get data from experience table 
+// Get data from experience table
 $sql = "SELECT title, description FROM experience WHERE resume_id = $resume_id";
-$stmt = $mysqli->prepare($sql);
-//$stmt->bind_param('i', $resume_id);
-$stmt->execute();
-$result = $stmt->get_result();
+$result = $mysqli->query($sql);
+$experienceData = array(); // Create an array to store experience data
 if ($result->num_rows > 0) {
-  $data = array();
-  $i = 0;
   while ($row = $result->fetch_assoc()) {
-    $data[$i++] = $row;
+    $experienceData[] = $row; // Populate the experienceData array
   }
 }
-$titleExp = array();
-$descriptionExp = array();
-for ($j = 0; $j < $i; $j++) {
-  $titleExp[$j] = $data[$j]['title'];
-  $descriptionExp[$j] = $data[$j]['description'];
-}
 
-echo "
-  <!DOCTYPE html>
+?>
+
+<!DOCTYPE html>
 <html lang='en'>
-  <head>
-    <meta charset='UTF-8' />
-    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    <title>Document</title>
-    <link rel='stylesheet' type='text/css' href='/tem4/tem4.css' />
-    <style>
+
+<head>
+  <meta charset='UTF-8' />
+  <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+  <title>Document</title>
+  <link rel='stylesheet' type='text/css' href='/tem4/tem4.css' />
+  <style>
     @media print {
       #printBtn {
         display: none;
       }
     }
   </style>
-  </head>
-  <body>
-    <script src='https://kit.fontawesome.com/b99e675b6e.js'></script>
-    <script>
-  function printDocument() {
-    window.print();
-    return false;
-  }
-</script>
+</head>
 
-    <div class='resume'>
-      <div class='resume_left'>
-        <div class='resume_profile'>
-          <img src='$avt' alt='profile_pic' />
-        </div>
-        <div class='resume_content'>
-          <div class='resume_item resume_info'>
-            <div class='title'>
-              <p class='bold'>$fullName</p>
-              <p class='regular'>$nameOfCV</p>
-            </div>
-            <ul>
-              <li>
-                <div class='icon'>
-                  <i class='fas fa-map-signs'></i>
-                </div>
-                <div class='data'>
-                  $address
-                </div>
-              </li>
-              <li>
-                <div class='icon'>
-                  <i class='fas fa-mobile-alt'></i>
-                </div>
-                <div class='data'>$phone</div>
-              </li>
-              <li>
-                <div class='icon'>
-                  <i class='fas fa-envelope'></i>
-                </div>
-                <div class='data'>$mail</div>
-              </li>
+<body>
+  <script src='https://kit.fontawesome.com/b99e675b6e.js'></script>
+  <script>
+    function printDocument() {
+      window.print();
+      return false;
+    }
+  </script>
 
-            </ul>
-          </div>
-          <div class='resume_item resume_skills'>
-            <div class='title'>
-              <p class='bold'>skill's</p>
-            </div>
-            <ul>
-              <li>
-                <div class='skill_name'>$skill</div>
-              </li>
-              
-            </ul>
-          </div>
-        </div>
+  <div class='resume'>
+    <div class='resume_left'>
+      <div class='resume_profile'>
+        <img src='<?= $avt ?>' alt='profile_pic' />
       </div>
-      <div class='resume_right'>
-        <div class='resume_item resume_about'>
+      <div class='resume_content'>
+        <div class='resume_item resume_info'>
           <div class='title'>
-            <p class='bold'>About me</p>
-          </div>
-          <p>
-           $intro
-          </p>
-        </div>
-        <div class='resume_item resume_work'>
-          <div class='title'>
-          <p class='bold'>Certificate</p>
-        </div>
-        <ul>
-            <li>
-              <div class='info'>
-                <p class='semi-bold'>$certificate</p>
-                
-              </div>
-            </li>
-            
-            
-          </ul>
-          <div class='resume_item resume_education'>
-          <div class='title'>
-            <p class='bold'>Work Experience</p>
+            <p class='bold'><?= $fullName ?></p>
+            <p class='regular'><?= $nameOfCV ?></p>
           </div>
           <ul>
             <li>
-              <div class='info'>
-                <p class='semi-bold'>$titleExp[0]</p>
-                <p>
-                  $descriptionExp[0]
-                </p>
+              <div class='icon'>
+                <i class='fas fa-map-signs'></i>
+              </div>
+              <div class='data'>
+                <?= $address ?>
               </div>
             </li>
             <li>
-              <div class='info'>
-                <p class='semi-bold'>$titleExp[1]</p>
-                <p>
-                $descriptionExp[1]
-                </p>
+              <div class='icon'>
+                <i class='fas fa-mobile-alt'></i>
               </div>
+              <div class='data'><?= $phone ?></div>
             </li>
-            
+            <li>
+              <div class='icon'>
+                <i class='fas fa-envelope'></i>
+              </div>
+              <div class='data'><?= $mail ?></div>
+            </li>
           </ul>
         </div>
-        </div>
-        <div class='resume_item resume_education'>
+        <div class='resume_item resume_skills'>
           <div class='title'>
-            <p class='bold'>Education</p>
+            <p class='bold'>Skills</p>
           </div>
           <ul>
             <li>
-              <div class='info'>
-                <p class='semi-bold'>$titleEdu[0]</p>
-                <p>
-                  $descriptionEdu[0]
-                </p>
-              </div>
-            </li>
-            <li>
-              <div class='info'>
-                <p class='semi-bold'>$titleEdu[1]</p>
-                <p>
-                $descriptionEdu[1]
-                </p>
-              </div>
+              <div class='skill_name'><?= $skill ?></div>
             </li>
           </ul>
         </div>
       </div>
     </div>
-    <div class='printCV' style='display:flex; justify-content:center;align-items:center;' > 
-  <a id='printBtn' style='padding: 20px 40px; background-color: #547638; text-decoration:none; border-radius:10px; color:#fff;' href='#' onclick='printDocument();'>Print PDF</a>
-</div>
-  
-  </body>
-</html>
+    <div class='resume_right'>
+      <div class='resume_item resume_about'>
+        <div class='title'>
+          <p class='bold'>About me</p>
+        </div>
+        <p>
+          <?= $intro ?>
+        </p>
+      </div>
+      <div class='resume_item resume_work'>
+        <div class='title'>
+          <p class='bold'>Certificate</p>
+        </div>
+        <ul>
+          <li>
+            <div class='info'>
+              <p class='semi-bold'><?= $certificate ?></p>
+            </div>
+          </li>
+        </ul>
+        <div class='resume_item resume_education'>
+          <div class='title'>
+            <p class='bold'>Work Experience</p>
+          </div>
+          <ul>
+            <?php foreach ($experienceData as $experience) { ?>
+              <li>
+                <div class='info'>
+                  <p class='semi-bold'><?= $experience['title'] ?></p>
+                  <p><?= $experience['description'] ?></p>
+                </div>
+              </li>
+            <?php } ?>
+          </ul>
+        </div>
+      </div>
+      <div class='resume_item resume_education'>
+        <div class='title'>
+          <p class='bold'>Education</p>
+        </div>
+        <ul>
+          <?php foreach ($educationData as $education) { ?>
+            <li>
+              <div class='info'>
+                <p class='semi-bold'><?= $education['title'] ?></p>
+                <p><?= $education['description'] ?></p>
+              </div>
+            </li>
+          <?php } ?>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class='printCV' style='display:flex; justify-content:center;align-items:center;'>
+    <a id='printBtn' style='padding: 20px 40px; background-color: #547638; text-decoration:none; border-radius:10px; color:#fff;' href='#' onclick='printDocument();'>Print PDF</a>
+  </div>
+</body>
 
-  
-";
+</html>
