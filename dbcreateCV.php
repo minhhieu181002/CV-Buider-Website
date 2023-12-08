1d1389e13115002aa2df2ce80d85542d0b4fe84d
JayNguyen168 <139840147+JayNguyen168@users.noreply.github.com>
2023-12-08 09:23:07 +0700

    Update avt_upload

--- dbcreateCV.php
+++ dbcreateCV.php
@@ -18,6 +18,38 @@ if ($mysqli->connect_error) {
 }
 $mysqli->set_charset("utf8mb4");
 
+//upload image
+if (!file_exists('uploads/')) {
+    mkdir('uploads/', 0777, true);
+}
+$file = $_FILES['file'];
+$fileName = $_FILES['file']['name'];
+$fileTmpName = $_FILES['file']['tmp_name'];
+$fileSize = $_FILES['file']['size'];
+$fileError = $_FILES['file']['error'];
+// $fileType = $_FILES['file']['type'];
+
+$fileExt = explode('.',$fileName);
+$fileActualExt = strtolower(end($fileExt));
+
+$allowed = array('jpg', 'jpeg', 'png');
+
+if(in_array($fileActualExt, $allowed)){
+    if($fileError===0){
+        if($fileSize < 500000){ //size < 500 kB
+            $fileNameNew = uniqid('',true).".".$fileActualExt;
+            $fileDestination = 'uploads/'.$fileNameNew;
+            move_uploaded_file($fileTmpName, $fileDestination);
+        } else {
+            echo "Your file is too big!";
+        }
+    } else {
+        echo "There was an error uploading your file!";
+    }
+} else {
+    echo "You can not files of this type!";
+}
+
 // Collect data from the form
 $userId = $_SESSION['id']; // Get the userId from the session
 $name = $_POST["name"];
